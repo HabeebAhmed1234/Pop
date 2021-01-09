4c493513415322a1f0eb6936524d0c88c420cefb
HabeebAhmed <1habeeb.ahmed1@gmail.com>
2021-01-09 15:18:55 -0500

    tweak

--- app/src/main/java/com/stupidfungames/pop/bubblespawn/bombbubbles/BombBubbleSpritePool.java
+++ app/src/main/java/com/stupidfungames/pop/bubblespawn/bombbubbles/BombBubbleSpritePool.java
@@ -28,7 +28,7 @@ public class BombBubbleSpritePool extends ItemPool {
   public static final AndengineColor WARN_BOMB_COLOUR = AndengineColor.YELLOW;
   public static final AndengineColor EXPLODING_BOMB_COLOUR = AndengineColor.RED;
   public static final float BOMB_BUBBLE_SIZE_DP = BubbleSize.MEDIUM.sizeDp;
-  public static final float BOMB_BUBBLE_COUNTDOWN_TEXT_TOP_PADDING_DP = 12;
+  public static final float BOMB_BUBBLE_COUNTDOWN_TEXT_TOP_PADDING_DP = 14;
   private final float bombBubbleSizePx;
 
   private final ItemInitializer<Sprite, BaseSpriteInitializerParams> initializer = new BaseSpriteItemInitializer<BaseSpriteInitializerParams>() {
