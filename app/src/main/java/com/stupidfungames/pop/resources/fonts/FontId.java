03babd35cbf9d8bcb3e8faed4786d067cb7c6477
HabeebAhmed <1habeeb.ahmed1@gmail.com>
2021-02-03 20:09:01 -0500

    scale text

--- app/src/main/java/com/stupidfungames/pop/resources/fonts/FontId.java
+++ app/src/main/java/com/stupidfungames/pop/resources/fonts/FontId.java
@@ -1,12 +1,8 @@
 package com.stupidfungames.pop.resources.fonts;
 
 public enum FontId {
-    MAIN_FONT,
     SCORE_TICKER_FONT,
-    TIMER_TICKER_FONT,
     INVENTORY_ICON_FONT,
-    COUNT_DOWN_FONT,
     NEON_FONT,
-    TOOLTIP_TEXT_FONT,
     BOMB_BUBBLE_COUNTDOWN_FONT
 }
--- app/src/main/java/com/stupidfungames/pop/resources/fonts/GameFontsManager.java
+++ app/src/main/java/com/stupidfungames/pop/resources/fonts/GameFontsManager.java
@@ -1,72 +1,66 @@
 package com.stupidfungames.pop.resources.fonts;
 
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.Typeface;
-
 import com.stupidfungames.pop.BaseEntity;
+import com.stupidfungames.pop.R;
 import com.stupidfungames.pop.binder.BinderEnity;
-
+import java.util.HashMap;
+import java.util.Map;
 import org.andengine.opengl.font.Font;
 import org.andengine.opengl.font.FontFactory;
 import org.andengine.opengl.font.FontManager;
 import org.andengine.opengl.texture.TextureManager;
-import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class GameFontsManager extends BaseEntity {
 
-    private final Map<FontId, Font> mFonts = new HashMap();
+  private final Map<FontId, Font> mFonts = new HashMap();
 
-    public GameFontsManager(BinderEnity parent) {
-        super(parent);
+  public GameFontsManager(BinderEnity parent) {
+    super(parent);
+  }
+
+  public Font getFont(FontId fontId) {
+    if (!mFonts.containsKey(fontId)) {
+      throw new IllegalArgumentException("Font " + fontId + " was never initialized");
     }
+    return mFonts.get(fontId);
+  }
 
-    public Font getFont(FontId fontId) {
-        if (!mFonts.containsKey(fontId)) {
-            throw new IllegalArgumentException("Font " + fontId + " was never initialized");
-        }
-        return mFonts.get(fontId);
+  @Override
+  public void onCreateResources() {
+    FontManager fontManager = get(FontManager.class);
+    TextureManager textureManager = get(TextureManager.class);
+    AssetManager assetManager = get(AssetManager.class);
+
+    Context context = get(Context.class);
+    Resources resources = context.getResources();
+
+    FontFactory.setAssetBasePath("font/");
+    mFonts.put(
+        FontId.SCORE_TICKER_FONT,
+        FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager, 256, 512),
+            assetManager, "neon.ttf", resources.getDimension(R.dimen.ticker_font_size), true,
+            Color.WHITE));
+    mFonts.put(
+        FontId.INVENTORY_ICON_FONT,
+        FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager, 256, 256),
+            assetManager, "neon.ttf", resources.getDimension(R.dimen.inventory_font_size), true,
+            Color.WHITE));
+    mFonts.put(
+        FontId.BOMB_BUBBLE_COUNTDOWN_FONT,
+        FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager, 256, 256),
+            assetManager, "neon.ttf", resources.getDimension(R.dimen.bomb_bubble_countdown_font),
+            true, Color.WHITE));
+    loadFonts();
+  }
+
+  private void loadFonts() {
+    for (Font font : mFonts.values()) {
+      font.load();
     }
-
-    @Override
-    public void onCreateResources() {
-        FontManager fontManager = get(FontManager.class);
-        TextureManager textureManager = get(TextureManager.class);
-        AssetManager assetManager= get(AssetManager.class);
-
-        mFonts.put(
-                FontId.MAIN_FONT,
-                FontFactory.create(fontManager, textureManager, 256, 256, TextureOptions.BILINEAR, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 48,true, Color.WHITE));
-        mFonts.put(
-                FontId.COUNT_DOWN_FONT,
-                FontFactory.create(fontManager, textureManager, 256, 256, TextureOptions.BILINEAR, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 100,true, Color.WHITE));
-
-        FontFactory.setAssetBasePath("font/");
-        mFonts.put(
-                FontId.SCORE_TICKER_FONT,
-                FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager,256,512), assetManager,"neon.ttf",80f,true, Color.WHITE));
-        mFonts.put(
-                FontId.TIMER_TICKER_FONT,
-                FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager,256,256), assetManager,"neon.ttf",80f,true, Color.WHITE));
-        mFonts.put(
-                FontId.INVENTORY_ICON_FONT,
-                FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager,256,256), assetManager,"neon.ttf",60f,true, Color.WHITE));
-        mFonts.put(
-                FontId.TOOLTIP_TEXT_FONT,
-                FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager,256,256), assetManager,"neon.ttf",60f,true, Color.WHITE));
-        mFonts.put(
-            FontId.BOMB_BUBBLE_COUNTDOWN_FONT,
-            FontFactory.createFromAsset(fontManager, new BitmapTextureAtlas(textureManager,256,256), assetManager,"neon.ttf",45f,true, Color.WHITE));
-        loadFonts();
-    }
-
-    private void loadFonts() {
-        for (Font font : mFonts.values()) {
-            font.load();
-        }
-    }
+  }
 }
